020d15f39a26a877d9f667a1ff158aa488d566df
vikas-yadav-2002 <vy348697@gmail.com>
2024-08-22 11:30:24 +0530

    bug found

--- packages/ui/src/code.tsx
+++ packages/ui/src/code.tsx
@@ -1,9 +1,7 @@
-export function Code({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
+export function Code({children, className} : {
+    children : React.ReactNode;
+    className? : string;
 }): JSX.Element {
-  return <code className={className}>{children}</code>;
+    return <code className={className}>
+        {children}</code>;
 }
